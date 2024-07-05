7348b7f0d8f24000069e3908a66e9e12f5b20456
hamidh-2k8 <hamidh.2k8@gmail.com>
2024-07-04 20:13:17 -0400

    Added TailwindCSS, Favicon. Will continue to work on design with NuxtUI

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,15 @@
+<script setup lang="ts">
+//import { NavigationHeader } from '#build/components';
+</script>
+
 <template>
     <div>
-        <div class="header-bar">
-            <div class="header-nav">
-                <div class="header-button">
-                    <nuxt-link to="/">Home</nuxt-link>
-                </div>
-                <div class="header-button">
-                    <nuxt-link to="/teams">Teams</nuxt-link>
-                </div>
-            </div>
-        </div>
-
         <div>
-            <slot />
+            <NavigationHeader />
+        </div>
+        <div><slot/></div>
+        <div>
+            <!--Footer?-->
         </div>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        <h1>FTC Open Alliance</h1>
-        <h2>This is the landing page for FTCOA. (Currently in testing)</h2>
+    <div class="">
+        <h1 class="my-5 text-5xl lg:text-6xl xl:text-7xl font-bold">
+            FTC Open Alliance
+        </h1>
+        <h2>
+            This is the landing page for FTCOA. (Currently in testing)
+        </h2>
     </div>
 </template>
 <script>
