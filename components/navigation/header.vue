814e32e014d04b7bbf55edbfa4c2d9521991ea7c
hamidh-2k8 <hamidh.2k8@gmail.com>
2024-07-06 17:02:15 -0400

    added CNAME + small changes

--- components/navigation/header.vue
+++ components/navigation/header.vue
@@ -1,13 +1,11 @@
 <template>
-    <div class="bg-black">
-        <div id="header-bar" class="flex justify-between bg-black p-5 m-4 shadow-lg shadow-orange-800 border-4 border-orange">
-            <div class="inline-flex justify-start max-w-36">
-                <img src="https://raw.githubusercontent.com/FTCOpenAlliance/brandkit/main/svg/SHORT-HORIZONTAL.svg">
-            </div>
-            <div id="header-nav" class="flex mx-5 *:rounded-none *:ml-2" >
-                <UButton label="Home" to="/" />
-                <UButton label="Teams" to="/teams"/>
-            </div>
+    <div id="header-bar" class="flex justify-between backdrop-blur-sm fixed top-0 left-0 right-0 p-5 m-4 shadow-lg shadow-orange-800 border border-orange">
+        <div class="inline-flex justify-start max-w-36">
+            <img src="https://raw.githubusercontent.com/FTCOpenAlliance/brandkit/main/svg/SHORT-HORIZONTAL.svg">
+        </div>
+        <div id="header-nav" class="flex mx-5 *:rounded-none *:ml-2" >
+            <UButton label="Home" to="/" />
+            <UButton label="Teams" to="/teams"/>
         </div>
     </div>
 </template>
@@ -16,7 +14,7 @@
 export default {
     setup () {
         
-
+        
         return {}
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="block items-center justify-center bg-[linear-gradient(to_right,#ff660044_2px,transparent_1px),linear-gradient(to_bottom,#ff660022_2px,#000_1px)] bg-[size:40px_40px] bg-clip-border sm:p-36 md:p-24 p-36">
-        <div class="sm:flex md:flex block items-center backdrop-blur-sm shadow-xl shadow-orange-800">
+    <div class="block items-center justify-center bg-[linear-gradient(to_right,#ff660044_2px,transparent_1px),linear-gradient(to_bottom,#ff660022_2px,#000_1px)] bg-[size:40px_40px] bg-clip-border lg:px-36 md:px-24 px-12 py-36">
+        <div class="sm:flex block items-center backdrop-blur-sm shadow-xl shadow-orange-800">
             <div class="mx-5 p-5">
                 <img src="https://raw.githubusercontent.com/FTCOpenAlliance/brandkit/main/svg/ICON.svg">
             </div>
@@ -8,7 +8,7 @@
                 <h1 class="my-5 sm:text-6xl text-4xl lg:text-7xl xl:text-8xl font-bold">
                     FTC Open Alliance
                 </h1>
-                <h2 class="text-lg">
+                <h2 class="text-lg pb-5">
                     The place for open and collaborative robotics in FIRST Tech Challenge.
                 </h2>
             </div>
