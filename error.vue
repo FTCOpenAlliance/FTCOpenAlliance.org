74fd2749514df495b781c44ee6e0e9261d957514
hamidh-2k8 <hamidh.2k8@gmail.com>
2024-07-06 22:29:38 -0400

    Added SEO and custom error page, fixed dark mode text bug

---
+++ error.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="float p-10 md:p-24 h-screen w-screen bg-[linear-gradient(to_right,#ff660044_1px,transparent_1px),linear-gradient(to_bottom,#ff660022_1px,#000_1px)] bg-[size:40px_40px]">
+
+        <div class="sm:p-36 p-12 backdrop-blur-sm shadow-xl shadow-orange">
+            <h1 class="text-8xl sm:text-9xl font-bold text-orange h-auto">{{ error.statusCode }}</h1>
+            <h2>{{ error.statusMessage }}</h2>
+            <UButton class="mt-10" label="Back to Home" to="/"/>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,29 @@
 <template>
     <div class="block items-center justify-center bg-[linear-gradient(to_right,#ff660044_2px,transparent_1px),linear-gradient(to_bottom,#ff660022_2px,#000_1px)] bg-[size:40px_40px] bg-clip-border lg:px-36 md:px-24 px-12 py-36">
-        <div class="sm:flex block items-center backdrop-blur-sm shadow-xl shadow-orange-800">
+        <div class="block sm:flex justify-center items-center backdrop-blur-sm shadow-xl shadow-orange-800">
             <div class="mx-5 p-5">
                 <img src="https://raw.githubusercontent.com/FTCOpenAlliance/brandkit/main/svg/ICON.svg">
             </div>
             <div class="mx-10">
-                <h1 class="my-5 sm:text-6xl text-4xl lg:text-7xl xl:text-8xl font-bold">
+                <h1 class="my-5 text-6xl md:text-7xl xl:text-8xl font-bold text-orange">
                     FTC Open Alliance
                 </h1>
-                <h2 class="text-lg pb-5">
+                <h2 class="text-xl pb-5">
                     The place for open and collaborative robotics in FIRST Tech Challenge.
                 </h2>
             </div>
         </div>
     </div>
 </template>
-<script>
-export default {
-    
-}
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'FTC Open Alliance',
+  ogTitle: 'FTC Open Alliance',
+  description: 'The place for open and collaborative robotics in FIRST Tech Challenge',
+  ogDescription: 'The place for open and collaborative robotics in FIRST Tech Challenge',
+  ogImage: 'https://raw.githubusercontent.com/FTCOpenAlliance/brandkit/main/png_4k/bg/ICON.png',
+})
 </script>
 <style>
     
