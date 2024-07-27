c320de01c9c51401f94e567c0e13a3048d74cb99
hamidh-2k8 <hamidh.2k8@gmail.com>
2024-07-27 15:36:42 -0400

    added API Endpoint fetching to teams list (testing cloudflare pages)

--- pages/teams/index.vue
+++ pages/teams/index.vue
@@ -6,14 +6,26 @@
             </h1>
         </PageTitle>
         <div class="md:p-24 p-12 grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-            <TeamCell teamname="Test Team" teamnumber="99999" buildthread="https://www.example.com/" cadlink="https://www.example.com/" codelink="https://www.example.com/" weblink="https://www.example.com/" photolink="https://www.example.com/" videolink="https://www.example.com/"/>
-            <TeamCell teamname="Test Team" teamnumber="99999" buildthread="https://www.example.com/" cadlink="https://www.example.com/" codelink="https://www.example.com/" weblink="https://www.example.com/" photolink="https://www.example.com/" videolink="https://www.example.com/"/>
-            <TeamCell teamname="Test Team" teamnumber="99999" buildthread="https://www.example.com/" codelink="https://www.example.com/" />
+            <TeamCell
+            v-for="data in linkData"
+            v-bind:teamnumber="data[0]"
+            v-bind:teamname="data[1]"
+            v-bind:weblink="'//' + data[2]"
+            v-bind:buildthread="'//' + data[3]"
+            v-bind:cadlink="'//' + data[4]"
+            v-bind:codelink="'//' + data[5]"
+            v-bind:photolink="'//' + data[6]"
+            v-bind:videolink="'//' + data[7]"/>
         </div>
     </div>
 </template>
-<script>
+<script setup>
+
+    let url = 'https://script.google.com/macros/s/AKfycbycFM4msB2ZJTt_vEGPi3gOL0EBeekRxUbH248JZExRoSHZmfbJZZTVtv1pBZyClfT1/exec'
+
+
+    const {data: teamLinks} = await useFetch(url, {
+        query: {request: 'getTeamLinks' }
+    })
+    const linkData = teamLinks._rawValue.data
 </script>
-<style lang="">
-    
-</style>
---
+++ pages/teams/redirect.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="flex justify-center items-center h-screen">
+            <h1 class="text-xl sm:text-4xl lg:text-6xl">Fetching Team Data...</h1>
+            
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'empty'
+})
+
+    navigateTo('/teams')
+
+// const link = useRoute().query.link?.toString()
+// window.console.log(link)
+
+</script>
+
+
+<style>
+
+</style>
