48013659b8943ca6d626c42268710e3df2cc4eb4
hamidh-2k8 <hamidh.2k8@gmail.com>
2024-09-17 12:36:30 -0400

    Better layout for team cell buttons

--- components/team/cell.vue
+++ components/team/cell.vue
@@ -9,17 +9,13 @@
                 </NuxtLink>
             </div>
             <div class="flex justify-end">
-                <div class="grid gap-2 grid-rows-2 sm:grid-cols-2 sm:grid-rows-1  lg:grid-rows-2 lg:grid-cols-1">
-                    <UButtonGroup class="*:rounded-none *:text-lg justify-end">
+                <div class="flex flex-wrap justify-end *:shrink *:rounded-none *:text-lg">
                         <UButton label="Build Thread" v-bind:to="normalizeUrl(buildthread)" v-if="checkNormalizable(buildthread)"/>
                         <UButton label="CAD" v-bind:to="normalizeUrl(cadlink)" v-if="checkNormalizable(cadlink)"/>
                         <UButton label="Code" v-bind:to="normalizeUrl(codelink)" v-if="checkNormalizable(codelink)"/>
-                    </UButtonGroup>
-                    <UButtonGroup class="*:rounded-none *:text-lg justify-end">
                         <UButton label="Photos" v-bind:to="normalizeUrl(photolink)" v-if="checkNormalizable(photolink)"/>
                         <UButton label="Videos" v-bind:to="normalizeUrl(videolink)" v-if="checkNormalizable(videolink)"/>
                         <UButton label="Website" v-bind:to="normalizeUrl(weblink)" v-if="checkNormalizable(weblink)"/>
-                    </UButtonGroup>
                 </div>
             </div>
         </div>
