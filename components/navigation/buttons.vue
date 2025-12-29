eff887ad8e721fe0165220dc1b6e693641c0f508
hamidh-2k8 <hamidh.2k8@gmail.com>
2025-12-28 23:03:21 -0500

    Added OA logos, state system for program, some changes for new backend. Still need to update signup form.

--- components/navigation/buttons.vue
+++ components/navigation/buttons.vue
@@ -1,12 +1,28 @@
 <template>
     
     <div class="flex flex-wrap justify-center gap-2" >
-        <UButton label="Teams" to="/teams"/>
+        <UButton v-if="program == 'ftc'" label="Teams" to="/ftc/teams"/>
+        <UButton v-if="program == 'frc'" label="Teams" to="/frc/teams"/>
+
+        <UButton v-if="program == 'generic'" label="Explore FTC" to="/ftc"/>
+        <UButton v-if="program == 'generic'" label="Explore FRC" to="/frc"/>
+
         <UButton label="Register/Update" to="/join"/>
         <UButton label="Archives" to="/archive"/>
-        <UButton label="Statistics" to="/stats"/>
+
+        <UButton v-if="program == 'ftc'" label="Statistics" to="/ftc/statistics"/>
+        <UButton v-if="program == 'frc'" label="Statistics" to="/frc/statistics"/>
+        
         <UButton label="Donate" icon="i-heroicons-outline-heart" target="_blank" to="https://hcb.hackclub.com/donations/start/ftcopenalliance" color="secondary"/>
-        <UButton label="FTCOA on CD" icon="i-heroicons-arrow-top-right-on-square-16-solid" target="_blank" to="https://www.chiefdelphi.com/c/first/ftc-open-alliance/" color="secondary"/>
+
+        <UButton v-if="program == 'ftc'" label="FTCOA on CD" icon="i-heroicons-arrow-top-right-on-square-16-solid" target="_blank" to="https://www.chiefdelphi.com/c/first/ftc-open-alliance/" color="secondary"/>
+        <UButton v-if="program == 'frc'" label="FRC on CD" icon="i-heroicons-arrow-top-right-on-square-16-solid" target="_blank" to="https://www.chiefdelphi.com/c/first/open-alliance/" color="secondary"/>
     </div>
-    
+
 </template>
+
+<script setup>
+    defineProps({
+        program: String
+    })
+</script>
--- error.vue
+++ error.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="float p-10 md:p-24 min-h-screen w-screen bg-grid">
+    <div class="flex p-10 md:p-24 h-screen w-screen items-center justify-center bg-red-950">
 
-        <div class="sm:p-36 p-12 backdrop-blur-xs shadow-xl shadow-primary">
-            <h1 class="text-8xl sm:text-9xl font-bold text-primary h-auto">{{ error?.statusCode }}</h1>
-            <h2 class="text-xl sm:text-3xl">{{ error?.statusMessage || '' }}</h2>
-            <details class="mt-5">
-                <summary class="text-lg text-primary cursor-pointer">Advanced</summary>
-                <p class="mt-5 text-primary-600">{{ error?.stack || '' }}</p>
-            </details>
-            <UButton class="mt-10 text-lg" label="Back to Home" to="/"/>
+        <div class="flex grow items-center gap-8 sm:p-36 p-12 bg-black/50 border-8 border-red-500">
+            <img src="/public/images/OALogos/Icon.svg" class="saturate-0"/>
+            <div class="flex flex-col grow gap-2">
+                <h1 class="text-8xl sm:text-9xl font-bold text-red-500 h-auto">{{ error?.statusCode }}</h1>
+                <h2 class="text-xl sm:text-3xl">{{ error?.statusMessage || '' }}</h2>
+                <UButton class="mt-10 text-lg" label="Back to Home" to="/"/>
+            </div>
         </div>
         
     </div>
@@ -20,6 +19,7 @@ import type { NuxtError } from '#app'
 const props = defineProps({
   error: Object as () => NuxtError
 })
+
 </script>
 
 <style scoped>
