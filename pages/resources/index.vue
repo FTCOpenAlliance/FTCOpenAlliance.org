a2f8845c7ddccb37124fbebb7c20f0da149f6869
hamidh-2k8 <hamidh.2k8@gmail.com>
2026-02-21 15:42:40 -0500

    Added QR Code and started a second template, as well as multiple formats/resolutions (SVG don't work)

--- pages/resources/index.vue
+++ pages/resources/index.vue
@@ -3,12 +3,10 @@
       <PageTitle title="Resources" />
 
       <div class="bg-dots flex flex-col gap-4 p-8 items-center w-full *:max-w-[80rem] *:w-full">
-        <PageBlock v-for="item in items" :key="item.title" :style="`
-            background: linear-gradient(to right, #000000bb, black), url(${item.image});
-            background-size: cover;
-            background-position: center;
-        `">
-            <div class="flex flex-col md:flex-row justify-between md:items-center gap-8">
+        <PageBlock v-for="item in items" :key="item.title" class="group overflow-clip">
+            <img class="absolute left-0 top-0 h-full w-full object-cover object-center -skew-x-12 scale-110 group-hover:scale-105 transition-transform" :src="item.image" />
+            <div class="absolute left-0 top-0 right-0 bottom-0 backdrop-blur-[1px] group-hover:backdrop-blur-xs bg-black/25 group-hover:bg-primary-500/25 transition-all bg-gradient-to-r from-[#00000067] to-black" />
+            <div class="z-10 top-0 bottom-0 flex flex-col md:flex-row justify-between md:items-center gap-8">
                 <div class="flex items-center gap-4">
                     <UIcon :name="item.icon" class="hidden md:block text-primary-400 min-w-12 size-12" />
                     <div class="flex flex-col">
--- pages/resources/templates.vue
+++ pages/resources/templates.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="bg-dots">
-        <PageTitle title="Ad Templates"/>
-        <PageBlock class="flex-row gap-16 justify-between items-center max-w-[80rem] my-16 mx-auto">
-            <div class="flex flex-col justify-start text-start max-w-[30rem]">
+    <div class="flex flex-col items-center bg-dots">
+        <PageTitle title="Ad Templates" class="w-full"/>
+        <PageBlock class="flex-col lg:flex-row gap-16 justify-between lg:items-center max-w-[80rem] mx-8 my-16">
+            <div class="flex flex-col justify-start text-start max-w-[30rem] gap-2">
                 <h2 class="text-primary-200 text-2xl font-bold">Create your Custom Team Ads!</h2>
                 <p class="text-lg">
                     Enter your team number and select your program to create
                     advertisements for your team's Open Alliance profile.
                 </p>
+                <div class="flex justify-between items-center gap-8">
+                    <p class="text-xl">Format:</p>
+                    <USelect v-model="imgFormat" class="w-full" :items="['PNG', 'JPG', 'SVG']" />
+                </div>
+                <div v-if="imgFormat != 'SVG'" class="flex justify-between items-center gap-8">
+                    <p class="text-xl">Resolution:</p>
+                    <URadioGroup v-model="imgResolution" orientation="horizontal" indicator="hidden" variant="card" :items="['MD', 'LG', 'XL']" />
+                </div>
             </div>
             <div class="flex flex-col gap-2">
                 <div class="flex justify-between items-center gap-8">
@@ -31,15 +39,15 @@
             </div>
         </PageBlock>
         <USeparator v-if="showAds" class="text-xl text-primary md:text-4xl px-16">Generated Ads</USeparator>
-        <div class="flex flex-col items-center p-16 gap-8">
+        <div class="flex flex-row flex-wrap items-center p-16 gap-8">
             <!-- 16:9 Ad -->
-            <div v-if="showAds" id="16x9_AD" class="relative w-[50rem] outline-2 outline-primary shadow-xl shadow-primary-400/50 aspect-video overflow-hidden">
+            <div v-if="showAds" id="16x9_AD" class="relative h-[28rem] outline-2 outline-primary shadow-xl shadow-primary-400/50 aspect-video overflow-hidden">
                 <img v-if="bgImage" :src="bgURL" class="right-0 absolute object-cover w-[28rem] h-full brightness-75" />
-                <div class="grid clip-slash absolute w-2/3 h-full shadow-2xl shadow-primary-400 border-4 border-primary-400 rotate-[-12deg] scale-150 translate-y-10 -translate-x-60"/>
+                <div class="bg-grid-static absolute w-2/3 h-full shadow-2xl shadow-primary-400 border-4 border-primary-400 rotate-[-12deg] scale-150 translate-y-10 -translate-x-60"/>
                 <div class="flex flex-col absolute justify-between h-full p-8">
-                    <div class="max-w-2/3 *:leading-none">
+                    <div class="max-w-1/2 *:leading-none">
                         <p class="text-3xl">Learn More About</p>
-                        <h2 class="text-primary text-[7rem] font-bold">{{ teamNumber }}</h2>
+                        <h2 class="text-primary text-[6rem] font-bold">{{ teamNumber }}</h2>
                         <p class="text-2xl">{{ teamData.TeamName }}</p>
                     </div>
                     <div class="max-w-1/2 *:leading-none">
@@ -47,11 +55,32 @@
                             <p class="text-primary text-2xl font-bold">ON</p>
                         </USeparator>
                         <img :src="`/images/${program}OALogos/TwoLineStacked.svg`"/>
-                        <img :src="qrURL" />
                     </div>
                 </div>
+                <div class="absolute right-15 h-full w-60 flex items-center">
+                    <Qrcode :value="qrURL" class="border-8 border-white" black-color="#000" white-color="#FFF"/>
+                </div>
                 <div class="absolute bottom-0 right-0 p-4">
-                    <UButton class="ad-download" variant="solid" icon="i-heroicons-arrow-down-tray" @click="downloadElement('16x9_AD')"/>
+                    <UButton label="Download" class="ad-download" variant="solid" icon="i-heroicons-arrow-down-tray" @click="downloadElement('16x9_AD')"/>
+                </div>
+            </div>
+            <!-- Insta Ad -->
+            <div v-if="showAds" id="INSTA_AD" class="relative h-[28rem] outline-2 outline-primary shadow-xl shadow-primary-400/50 aspect-square overflow-hidden">
+                <img v-if="bgImage" :src="bgURL" class="absolute object-cover w-full h-2/3 brightness-75" />
+                <div class="absolute flex items-center justify-between top-0 w-full h-20 p-4 bg-[linear-gradient(to_bottom,#000000_0%,#00000000_100%)]">
+                    <img class="object-contain h-10" :src="`/images/${program}OALogos/TwoLineStacked.svg`"/>
+                    <p class="text-2xl font-bold">TEAM HIGHLIGHT</p>
+                </div>
+                <div class="bg-grid-static absolute flex p-4 items-center justify-between bottom-0 w-full h-1/3 shadow-2xl shadow-primary-400 border-t-4 border-primary-400">
+                    <div class="*:leading-none gap-2">
+                        <p class="text-lg">{{ teamData.TeamName }}</p>
+                        <h2 class="text-primary text-[4rem] font-bold">{{ teamNumber }}</h2>
+                        <p class="text-primary-600">{{ teamData.Location }}</p>
+                    </div>
+                    <Qrcode class="w-30 border-8 border-white" :value="qrURL" black-color="#000" white-color="#FFF"/>
+                </div>
+                <div class="absolute bottom-0 right-0 p-4">
+                    <UButton label="Download" class="ad-download" variant="solid" icon="i-heroicons-arrow-down-tray" @click="downloadElement('INSTA_AD')"/>
                 </div>
             </div>
         </div>
@@ -60,7 +89,7 @@
 
 <script setup>
 import { Program } from '~/assets/scripts/programs'
-import { domToPng } from 'modern-screenshot'
+import { domToPng, domToJpeg, domToSvg, domToForeignObjectSvg } from 'modern-screenshot'
 
 const apiURL = useRuntimeConfig().public.API_URL
 let toast = useToast()
@@ -72,11 +101,13 @@ const bgImage = ref()
 const bgURL = computed(() => {
     return bgImage.value ? URL.createObjectURL(bgImage.value) : null
 })
-const qrURL = ref(null)
+const qrURL = computed(() =>  teamNumber.value && teamData.value ? `https://theopenalliance.org/${program.value.toLowerCase()}/teams/${teamNumber.value}` : null)
+const imgFormat = ref("PNG")
+const imgResolution = ref("LG")
 
 // TESTING VARIABLES!
 // program.value = Program.FTC
-// const teamNumber = ref(12736)
+// const teamNumber = ref(6347)
 // const teamData = ref(null)
 // const bgImage = ref(new Blob())
 // const bgURL = ref("https://www.firstinspires.org/hubfs/image-library/ftc-20250416ben00346.jpg")
@@ -128,20 +159,41 @@ async function downloadElement(name) {
         return
     }
 
-    domToPng(element, {scale: 2, quality: 1, filter: (node) => !(node.classList && node.classList.contains('ad-download'))})
+    let scale
+
+    if (imgResolution.value === "XL") {
+        scale = 4.8
+    } else if (imgResolution.value === "LG") {
+        scale = 3.6
+    } else {
+        scale = 2.4
+    }
+
+    domToImage(element, {scale: scale, quality: 1, filter: (node) => !(node.classList && node.classList.contains('ad-download'))})
         .then((dataurl) => {
             const link = document.createElement('a')
             link.href = dataurl
-            link.download = `${teamNumber.value}_${name}.png`
+            link.download = `${teamNumber.value}_${name}_${imgResolution.value}.${imgFormat.value.toLowerCase()}`
             link.click()
         })
+}
+
+async function domToImage(element, options) {
+    options.type = `image/${imgFormat.value.toLowerCase()}`
+    if (imgFormat.value === 'SVG') {
+        return await domToForeignObjectSvg(element, options)
+    } else if (imgFormat.value === 'PNG') {
+        return await domToPng(element, options)
+    } else if (imgFormat.value === 'JPG') {
+        return await domToJpeg(element, options)
+    }
 }
 
 </script>
 
 <style>
 
-.grid {
+.bg-grid-static {
     background-image: linear-gradient(to right, transparent 60%, #000000aa 100%),linear-gradient(to right, var(--color-primary-800) 1px,transparent 1px),linear-gradient(to bottom,var(--color-primary-800) 1px,var(--color-primary-900) 1px);
     background-size: 100% 100%, 20px 20px, 20px 20px;
 }
